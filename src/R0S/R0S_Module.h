a1b62d72ba4b8d452aaa7a19b51e0c11c7230355
LA_DI_DA <11174151+0O0o0oOoO00@users.noreply.github.com>
2024-02-04 23:10:59 +0800

    Add: enum module

--- src/R0S/R0S_Module.h
+++ src/R0S/R0S_Module.h
@@ -1,4 +1,13 @@
 #pragma once
 #include <ntifs.h>
+#include "R0S_NtoskrnlStruct.h"
+#include "R0S_Define.h"
+
+typedef ENUM_STATUS (*ENUM_MODULE)(_In_ PKLDR_DATA_TABLE_ENTRY pKldrDataTableEntry, _In_opt_ PVOID Parameter);
+
+NTSTATUS EnumModule(
+	_In_ ENUM_MODULE fnCallback,
+	_In_opt_ PVOID Parameter
+);
 
 PVOID GetNtoskrnlBase();
